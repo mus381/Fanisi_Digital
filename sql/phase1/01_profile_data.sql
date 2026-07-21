4c2b8e358cb59bc22322ceaad9b483812bc37f6e
mus381 <M82515661@gmail.com>
2026-07-22 00:57:58 +0300

    Add initial data profiling

---
+++ sql/phase1/01_profile_data.sql
@@ -0,0 +1,10 @@
+-- ==================================================
+-- Fanisi Digital
+-- Phase 1
+-- Data Profiling
+-- ==================================================
+
+-- Total number of rows
+
+SELECT COUNT(*) AS total_rows
+FROM raw_invoices;
